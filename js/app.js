ff633d4022e7e7c34e9a9851e9d3f3391b7db2c4
Alvaro <alvarogr16@gmail.com>
2019-03-20 21:27:09 -0500

    nuevo
    hoy

--- js/app.js
+++ js/app.js
@@ -1,18 +1,2 @@
 $(document).foundation()
 
-$(document).ready(function(){
-
-      var height = $(window).height();
-
-      $('#first').height(height);
-      $('#second').height(height);
-      $('#third').height(height);
-});
-
-$(window).resize(function(){
-	var height = $(window).height();
-
-      $('#first').height(height);
-      $('#second').height(height);
-      $('#third').height(height);
-})
