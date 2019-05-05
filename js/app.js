5b892ac6021ebbd9435c1e6756137f97a0205263
Alvaro <alvarogr16@gmail.com>
2019-05-04 20:00:24 -0500

    actualizaciones
    Terminadas

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
 $(document).foundation()
 
+jQuery(document).ready(function() {
+		var duration = 500;
+		jQuery(window).scroll(function() {
+			if (jQuery(this).scrollTop() > 200) {
+				jQuery('.to-top').fadeIn(duration);
+			} else {
+				jQuery('.to-top').fadeOut(duration);
+			}
+		});
+
+		jQuery('.to-top').click(function(event) {
+			event.preventDefault();
+			jQuery('html').animate({scrollTop: 0}, duration);
+			return false;
+		})
+	});
